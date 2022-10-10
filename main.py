ca3e7bc3505645e74c38c47ea0f78e96abb10790
Laura Ottevanger <laura.ottevanger@hotmail.com>
2022-10-10 15:30:51 +0200

    added translation and dictionaries in separate file

--- main.py
+++ main.py
@@ -25,8 +25,6 @@ from bokeh.models import (
     HoverTool,
     EqHistColorMapper
 )
-import colorcet as cc
-from bokeh.palettes import Turbo256, Reds256, Greens256, Inferno256, Magma256, Plasma256, Greys256
 import os
 
 import json
@@ -34,6 +32,7 @@ from collections import OrderedDict
 
 import cbsodata
 import warnings
+from dictionaries import typelist, colourlist, crime_subsetlist, chidict, yearlist
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=UserWarning)
@@ -56,71 +55,6 @@ del df['name']
 columns = sorted(df.columns)
 discrete = [x for x in columns if df[x].dtype == object]
 continuous = [x for x in columns if x not in discrete]
-
-yearlist = ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"]
-crime_subsetlist = ["TotaalGeregistreerdeMisdrijven_1", "GeregistreerdeMisdrijvenRelatief_2",
-                    "GeregistreerdeMisdrijvenPer1000Inw_3", "TotaalOpgehelderdeMisdrijven_4",
-                    "OpgehelderdeMisdrijvenRelatief_5", "RegistratiesVanVerdachten_6"]
-typelist = ['Misdrijven, totaal', 'CHI value', '1 Vermogensmisdrijven', '11 Diefstal/verduistering en inbraak',
-            '111 Diefstal en inbraak met geweld', '112 Diefstal en inbraak zonder geweld', '12 Bedrog',
-            '121 Oplichting',
-            '122 Flessentrekkerij', '123 Bedrog (overig)', '13 Valsheidsmisdrijven', '131 Muntmisdrijf',
-            '132 Valsheid in zegels en merken', '133 Valsheid in geschriften', '14 Heling',
-            '15 Afpersing en afdreiging',
-            '16 Bankbreuk', '17 Witwassen', '18 Vermogensmisdrijf (overig)', '2 Vernielingen,misdropenborde/gezag',
-            '21 Vernieling en beschadiging', '211 Vernieling aan auto', '212 Vernieling aan openbaar gebouw',
-            '213 Vernieling middel openb vervoer', '215 Vernieling, beschadiging (overig)', '22 Openbare orde misdrijf',
-            '221 Openlijke geweldpleging', '2211 Openlijk geweld tegen persoon', '2212 Openlijke geweld tegen goed',
-            '222 Huisvredebreuk', '223 Lokaalvredebreuk', '224 Computervredebreuk', '225 Discriminatie',
-            '226 Openbare orde misdrijf (overig)', '23 Brandstichting / ontploffing', '24 Openbaar gezag misdrijf',
-            '241 Niet opvolgen van ambtelijk bevel', '242 Wederspannigheid', '243 Valse aangifte',
-            '245 Verblijf ongewenste vreemdeling', '246 Openbaar gezag misdrijf (overig)',
-            '3 Gewelds- en seksuele misdrijven', '31 Mishandeling', '32 Bedreiging en stalking', '321 Bedreiging',
-            '322 Stalking', '33 Seksueel misdrijf', '331 Aanranding', '332 Verkrachting',
-            '333 Schennis der eerbaarheid',
-            '334 Ontucht met minderjarige', '335 Pornografie', '336 Ontucht met misbruik van gezag',
-            '337 Seksueel misdrijf (overig)', '34 Levensmisdrijf', '35 Vrijheidsbeneming/gijzeling',
-            '36 Mensenhandel en 244 mensensmokkel',
-            '37 Geweldsmisdrijf (overig)', '4 Misdrijven WvSr (overig)', '5 Verkeersmisdrijven',
-            '51 Verlaten plaats ongeval', '52 Rijden onder invloed', '53 Rijden tijdens ontzegging besturen',
-            '54 Rijden tijdens rijverbod', '55 Voeren vals kenteken', '56 Joyriding',
-            '57 Weigeren blaastest/bloedonderzoek ed', '58 Verkeersmisdrijf (overig)', '6 Drugsmisdrijven',
-            '61 Harddrugs', '62 Softdrugs', '6.3 Drugsmisdrijf (overig)', '7 Vuurwapenmisdrijven',
-            '9 Misdrijven overige wetten', '91 Militaire misdrijven', '92 Misdrijven (overig)']
-
-chidict = {
-    'Misdrijven, totaal': 0, '1 Vermogensmisdrijven': 0, '11 Diefstal/verduistering en inbraak': 0,
-    '111 Diefstal en inbraak met geweld': 3243, '112 Diefstal en inbraak zonder geweld': 856, '12 Bedrog': 0,
-    '121 Oplichting': 1236, '122 Flessentrekkerij': 0, '123 Bedrog (overig)': 871, '13 Valsheidsmisdrijven': 0,
-    '131 Muntmisdrijf': 1769, '132 Valsheid in zegels en merken': 1221, '133 Valsheid in geschriften': 856,
-    '14 Heling': 1601, '15 Afpersing en afdreiging': 2148, '16 Bankbreuk': 1601, '17 Witwassen': 1236,
-    '18 Vermogensmisdrijf (overig)': 0, '2 Vernielingen,misdropenborde/gezag': 0, '21 Vernieling en beschadiging': 0,
-    '211 Vernieling aan auto': 491, '212 Vernieling aan openbaar gebouw': 856,
-    '213 Vernieling middel openb vervoer': 674, '215 Vernieling, beschadiging (overig)': 755,
-    '22 Openbare orde misdrijf': 0, '221 Openlijke geweldpleging': 0, '2211 Openlijk geweld tegen persoon': 2696,
-    '2212 Openlijke geweld tegen goed': 1221, '222 Huisvredebreuk': 233, '223 Lokaalvredebreuk': 71,
-    '224 Computervredebreuk': 309, '225 Discriminatie': 390, '226 Openbare orde misdrijf (overig)': 938,
-    '23 Brandstichting / ontploffing': 5601, '24 Openbaar gezag misdrijf': 0,
-    '241 Niet opvolgen van ambtelijk bevel': 71, '242 Wederspannigheid': 1146, '243 Valse aangifte': 233,
-    '245 Verblijf ongewenste vreemdeling': 142, '246 Openbaar gezag misdrijf (overig)': 1120,
-    '3 Gewelds- en seksuele misdrijven': 0, '31 Mishandeling': 1221, '32 Bedreiging en stalking': 0,
-    '321 Bedreiging': 187, '322 Stalking': 674, '33 Seksueel misdrijf': 0, '331 Aanranding': 1966,
-    '332 Verkrachting': 2696, '333 Schennis der eerbaarheid': 71, '334 Ontucht met minderjarige': 856,
-    '335 Pornografie': 81, '336 Ontucht met misbruik van gezag': 1221, '337 Seksueel misdrijf (overig)': 1586,
-    '34 Levensmisdrijf': 5981, '35 Vrijheidsbeneming/gijzeling': 5981, '36 Mensenhandel en 244 mensensmokkel': 3243,
-    '37 Geweldsmisdrijf (overig)': 2864, '4 Misdrijven WvSr (overig)': 1120, '5 Verkeersmisdrijven': 0,
-    '51 Verlaten plaats ongeval': 309, '52 Rijden onder invloed': 309, '53 Rijden tijdens ontzegging besturen': 309,
-    '54 Rijden tijdens rijverbod': 96, '55 Voeren vals kenteken': 142, '56 Joyriding': 142,
-    '57 Weigeren blaastest/bloedonderzoek ed': 96, '58 Verkeersmisdrijf (overig)': 1146, '6 Drugsmisdrijven': 0,
-    '61 Harddrugs': 2316, '62 Softdrugs': 755, '6.3 Drugsmisdrijf (overig)': 755, '7 Vuurwapenmisdrijven': 1586,
-    '9 Misdrijven overige wetten': 0, '91 Militaire misdrijven': 0, '92 Misdrijven (overig)': 0
-}
-
-colourlist = {
-    'Blue': cc.blues, 'Red': Reds256[::-1], 'Green': Greens256[::-1], 'Grey': Greys256[::-1], 'Rainbow': cc.CET_R1,
-    'Turbo': Turbo256,
-    'Inferno': Inferno256[::-1], 'Magma': Magma256[::-1], 'Plasma': Plasma256[::-1], 'Fridge': cc.CET_L19
-}
 
 
 def importdata(reload_s):
@@ -197,8 +131,8 @@ def add_chi(df_crimes_s, reload_s):
 
         frames = [df_crimes_s, df_chi]
         df_crime = pd.concat(frames)
-        for i in range (1, 6):
-            df_crime.drop([str('CHI_'+str(i))], axis=1)
+        for i in range(1, 6):
+            df_crime.drop([str('CHI_' + str(i))], axis=1)
         df_crime = df_crime.fillna(0)
         df_crime.to_pickle("chidf.pkl")
     else:
@@ -264,8 +198,7 @@ def create_figure(dutch_municipalities_dict_s, palette_s):
     TOOLS = "pan,wheel_zoom,box_zoom,reset,hover,save"
 
     p = bokeh.plotting.figure(
-        background_fill_color="white", title=str("Crimes per municipality. "+year+ ", "+
-                                                 type_of_crimes+ ", "+crime_subset), tools=TOOLS,
+        background_fill_color="white", title=str("Crimes per municipality. " + year + ", " + str(list(typelist.keys())[list(typelist.values()).index(type_of_crimes)]) + ", " + str(list(crime_subsetlist.keys())[list(crime_subsetlist.values()).index(crime_subset)])), tools=TOOLS,
         x_axis_location=None, y_axis_location=None
     )
 
@@ -310,9 +243,9 @@ def update(attr, old, new):
     if new in yearlist:
         year = new
     elif new in crime_subsetlist:
-        crime_subset = new
+        crime_subset = crime_subsetlist[new]
     elif new in typelist:
-        type_of_crimes = new
+        type_of_crimes = typelist[new]
     elif new in ["Yes", "No"]:
         reload = new
     elif new in colourlist:
@@ -353,10 +286,11 @@ create_figure(dutch_municipalities_dict, palette)
 year_sel = Select(title='Year', value='2017', options=yearlist)
 year_sel.on_change('value', update)
 
-crime_subset_sel = Select(title='Crime subset', value='TotaalGeregistreerdeMisdrijven_1', options=crime_subsetlist)
+crime_subset_sel = Select(title='Crime subset', value='TotaalGeregistreerdeMisdrijven_1',
+                          options=list(crime_subsetlist.keys()))
 crime_subset_sel.on_change('value', update)
 
-type_of_crime_sel = Select(title='Type of crime', value='Misdrijven, totaal', options=typelist)
+type_of_crime_sel = Select(title='Type of crime', value='Misdrijven, totaal', options=list(typelist.keys()))
 type_of_crime_sel.on_change('value', update)
 
 reload_sel = Select(title='Reload: Warning, will reload data and will take a long time', value='No',
